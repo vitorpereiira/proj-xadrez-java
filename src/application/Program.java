cec7c6876b68604cf592819602922ea10361f0b4
Vitor Pereira <vitor.we@hotmail.com>
2020-05-22 16:19:23 -0300

    Camada do tabuleiro e imprimindo o mesmo.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hello world");
 
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 	}
 
 }
