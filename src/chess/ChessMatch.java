71454cba7aec8248423126f5cce1b455cdcf0c49
Vitor Pereira <vitor.we@hotmail.com>
2020-05-26 03:15:48 -0300

    Possivel movimentos das pecas.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Nao ha nenhuma peca na posicao de origem.");
 		}
+		if (board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Nao ha movimentos possiveis para a peca escolhida.");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
