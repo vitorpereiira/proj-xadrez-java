46d51d694bb1e6f0716fdd3e9eaaff7e530b8f4f
Vitor Pereira <vitor.we@hotmail.com>
2020-05-26 02:24:12 -0300

    Tratamento de excecoes e tela de limpeza.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,38 @@
 package application;
 
+import java.util.Scanner;
+
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
-
+		Scanner leia = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
 		
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(leia);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(leia);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				
+			}
+		}
 	}
 
 }
