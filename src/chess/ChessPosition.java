302f89af67e5f9c854bfa05cc53101dc0ab1b52a
Vitor Pereira <vitor.we@hotmail.com>
2020-05-26 22:26:09 -0300

    Logica de check.

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,7 @@ public class ChessPosition {
 	private int row;
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Erro ao instaciar a posicao.");
+			throw new ChessException("Erro ao instaciar a posicao. Posicao valida de a1 a h8.");
 		}
 		this.column = column;
 		this.row = row;
@@ -25,7 +25,7 @@ public class ChessPosition {
 	}
 	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
